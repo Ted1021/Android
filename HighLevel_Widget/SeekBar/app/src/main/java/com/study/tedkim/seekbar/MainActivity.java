805a361a0fa367f97f2915ba0b4b4bb74c1ee226
Ted <kimtx2017@gmail.com>
2017-03-29 17:22:30 +0900

    # Implement Seek Bar

--- HighLevel_Widget/SeekBar/app/src/main/java/com/study/tedkim/seekbar/MainActivity.java
+++ HighLevel_Widget/SeekBar/app/src/main/java/com/study/tedkim/seekbar/MainActivity.java
@@ -1,13 +1,51 @@
 package com.study.tedkim.seekbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar sbPercent;
+    TextView tvPercent;
+
+    int mPercent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
+
+    public void initView(){
+
+        tvPercent = (TextView) findViewById(R.id.textView_percent);
+
+        sbPercent = (SeekBar) findViewById(R.id.seekBar_percent);
+
+        // setOnSeekBarChangeListener() 구현
+        sbPercent.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mPercent = progress;
+                tvPercent.setText(mPercent+""+" %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
 }
