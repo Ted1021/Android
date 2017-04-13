88f19b7be3e2816614fb22c99e9c169695205c13
Ted <kimtx2017@gmail.com>
2017-04-14 00:10:10 +0900

    # Set Permission for ‘ACTION_CALL’

--- Activity/ImplicitIntent/app/src/main/java/com/study/tedkim/implicitintent/MainActivity.java
+++ Activity/ImplicitIntent/app/src/main/java/com/study/tedkim/implicitintent/MainActivity.java
@@ -1,12 +1,16 @@
 package com.study.tedkim.implicitintent;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button btnCall = (Button) findViewById(R.id.button_call);
-        btnCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-            }
-        });
+        btnCall.setOnClickListener(this);
 
         Button btnDial = (Button) findViewById(R.id.button_dial);
         btnDial.setOnClickListener(this);
@@ -32,18 +31,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        switch(v.getId()){
+        Intent intent;
+
+        switch (v.getId()) {
 
             case R.id.button_call:
 
-                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent = new Intent(Intent.ACTION_CALL);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+
                 break;
 
             case R.id.button_dial:
 
+                intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+
                 break;
 
             case R.id.button_contact:
+
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setData(Uri.parse("content://contacts/phones"));
+                startActivityForResult(i,0);
 
                 break;
 
