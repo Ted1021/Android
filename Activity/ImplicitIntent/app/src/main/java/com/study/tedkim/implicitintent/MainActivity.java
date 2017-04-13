66ad5dd88834b0f5a55b82a63548f9d0edef77c8
Ted <kimtx2017@gmail.com>
2017-04-13 23:59:01 +0900

    # Implement Implicit Intent

--- Activity/ImplicitIntent/app/src/main/java/com/study/tedkim/implicitintent/MainActivity.java
+++ Activity/ImplicitIntent/app/src/main/java/com/study/tedkim/implicitintent/MainActivity.java
@@ -1,13 +1,54 @@
 package com.study.tedkim.implicitintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCall = (Button) findViewById(R.id.button_call);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+            }
+        });
+
+        Button btnDial = (Button) findViewById(R.id.button_dial);
+        btnDial.setOnClickListener(this);
+
+        Button btnContact = (Button) findViewById(R.id.button_contact);
+        btnContact.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.button_call:
+
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                break;
+
+            case R.id.button_dial:
+
+                break;
+
+            case R.id.button_contact:
+
+                break;
+
+
+        }
+
     }
 }
