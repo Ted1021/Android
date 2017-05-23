56584845da997e68b4ad73eca2e8012bf20693aa
Ted <kimtx2017@gmail.com>
2017-05-23 17:45:19 +0900

    # Implements File Explorer

---
+++ File/File_Managing/app/src/main/java/com/study/tedkim/file_managing/MainActivity.java
@@ -0,0 +1,143 @@
+package com.study.tedkim.file_managing;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRoot, btnUp;
+    TextView tvCurrentPath;
+
+    RecyclerView rvDirList;
+    RecyclerViewAdapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+
+    ArrayList<String> mDataSet = new ArrayList<>();
+
+    String mRoot;
+    public static String mCurrentPath;
+
+    public static Handler mHandler;
+    public static int EVENT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        setRecyclerView();
+
+        initData();
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if(msg.what == EVENT){
+                    refreshFiles();
+                }
+            }
+        };
+    }
+
+    public void initView(){
+
+        tvCurrentPath = (TextView) findViewById(R.id.textView_currentPath);
+
+        btnRoot = (Button) findViewById(R.id.button_root);
+        btnRoot.setOnClickListener(this);
+
+        btnUp = (Button) findViewById(R.id.button_up);
+        btnUp.setOnClickListener(this);
+
+    }
+
+    public void initData(){
+
+        mRoot = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mCurrentPath = mRoot;
+
+        refreshFiles();
+    }
+
+    public void setRecyclerView(){
+
+        rvDirList = (RecyclerView) findViewById(R.id.recyclerView_cardDir);
+
+        mAdapter = new RecyclerViewAdapter(this, R.layout.list_layout, mDataSet);
+        rvDirList.setAdapter(mAdapter);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        rvDirList.setLayoutManager(mLayoutManager);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            case R.id.button_root:
+
+                mCurrentPath = mRoot;
+                refreshFiles();
+
+                break;
+
+            case R.id.button_up:
+
+                if(!mCurrentPath.equals(mRoot)){
+
+                    int end = mCurrentPath.lastIndexOf("/");
+                    mCurrentPath = mCurrentPath.substring(0, end);
+
+                    refreshFiles();
+                }
+                else
+                    Toast.makeText(MainActivity.this, "This Directory is ROOT", Toast.LENGTH_SHORT).show();
+
+                break;
+
+        }
+    }
+
+    public void refreshFiles(){
+
+        tvCurrentPath.setText(mCurrentPath);
+        mDataSet.clear();
+
+        File currentState = new File(mCurrentPath);
+        String[] files = currentState.list();
+        if(files != null){
+            for(int i=0; i<files.length; i++){
+
+                String path = mCurrentPath + "/" + files[i];
+                String name;
+
+                File file = new File(path);
+                if(file.isDirectory()){
+                    name = "[" + files[i] + "]";
+                }
+                else
+                    name = files[i];
+
+                mDataSet.add(name);
+            }
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+}
---
+++ File/File_Managing/app/src/main/java/com/study/tedkim/file_managing/RecyclerViewAdapter.java
@@ -0,0 +1,95 @@
+package com.study.tedkim.file_managing;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by tedkim on 2017. 5. 23..
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    Context mContext;
+    int mLayout;
+    ArrayList<String> mDataSet = new ArrayList<>();
+    LayoutInflater mInflater;
+
+    public RecyclerViewAdapter(Context context, int layout, ArrayList<String> dataSet){
+
+        mContext = context;
+        mLayout = layout;
+        mDataSet = dataSet;
+
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvIndex, tvDir;
+        LinearLayout item;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvIndex = (TextView) itemView.findViewById(R.id.textView_index);
+            tvDir = (TextView) itemView.findViewById(R.id.textView_dir);
+
+            item = (LinearLayout) itemView.findViewById(R.id.linearLayout_item);
+
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = mInflater.inflate(mLayout, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+        holder.tvIndex.setText(position+"");
+        holder.tvDir.setText(mDataSet.get(position));
+        holder.item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(mContext, position+" is Clicked!!", Toast.LENGTH_SHORT).show();
+                // TODO - item click listener interface 구현 해 볼 것.
+                // TODO - 이걸 Handler 로 하다니 ㅋㅋㅋㅋㅋㅋㅋ
+                String path = mDataSet.get(position);
+                File dir = new File(path);
+                if(!dir.isDirectory()){
+
+                    path = path.substring(1, path.length()-1);
+                    MainActivity.mCurrentPath = path;
+
+                    Toast.makeText(mContext, path, Toast.LENGTH_SHORT).show();
+
+                    MainActivity.mHandler.sendEmptyMessage(MainActivity.EVENT);
+                }
+                else
+                    Toast.makeText(mContext, dir.getAbsolutePath() , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+}
