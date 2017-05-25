00b3e3695c511dcee07ec3445e8f1cdd2eb674b9
Ted <kimtx2017@gmail.com>
2017-05-25 11:41:40 +0900

    # Add comment
    - add comment on preference fragment

--- File/PreferenceFragment/app/src/main/java/com/study/tedkim/preferencefragment/SettingFragment.java
+++ File/PreferenceFragment/app/src/main/java/com/study/tedkim/preferencefragment/SettingFragment.java
@@ -8,6 +8,8 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 /**
  * A simple {@link Fragment} subclass.
  */
+
+// Preference Fragment 를 사용하기 위해서는 'PreferenceFragmentCompat' 을 상속 받아야 한다
 public class SettingFragment extends PreferenceFragmentCompat {
 
 
@@ -15,6 +17,8 @@ public class SettingFragment extends PreferenceFragmentCompat {
         // Required empty public constructor
     }
 
+    // 단순히 'onCreate' 생명주기에서
+    // res 폴더에 미리 준비해 둔 XML 파일을 preference 로 등록해 준다
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.setting);
