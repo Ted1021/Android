80eb9cf924d3be2db595c8fbdcb528c5ecbff3a9
Ted <kimtx2017@gmail.com>
2017-06-05 14:55:50 +0900

    # Add AsyncTask
    - implement AsyncTask in 'downloadHtml()'

--- Network/HttpUrlConnection/app/src/main/java/com/study/tedkim/httpurlconnection/MainActivity.java
+++ Network/HttpUrlConnection/app/src/main/java/com/study/tedkim/httpurlconnection/MainActivity.java
@@ -1,10 +1,12 @@
 package com.study.tedkim.httpurlconnection;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -24,10 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         initView();
-
     }
 
-    public void initView(){
+    public void initView() {
 
         tvHtml = (TextView) findViewById(R.id.textView_html);
 
@@ -36,51 +37,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    public String downloadHtml(){
+    public void downloadHtml() {
 
-        StringBuilder html = new StringBuilder();
+        new AsyncTask<Void, String, String>(){
 
-        try {
-            URL url = new URL("http://www.google.com");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // initView
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
 
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-            if(responseCode == HTTP_OK){
-
-                InputStreamReader isr = new InputStreamReader(connection.getInputStream());
-                BufferedReader br = new BufferedReader(isr);
-
-                String stringLine;
-                while((stringLine = br.readLine()) != null){
-
-                    html.append(stringLine);
-                }
-
-                return html.toString();
+                // TODO - Progress Dial Set
             }
 
-        }catch(Exception e){
+            // Background process using thread
+            @Override
+            protected String doInBackground(Void... params) {
 
-            e.printStackTrace();
-        }
-        return null;
+                try{
+
+                    URL url = new URL("http://www.google.com");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    connection.setRequestMethod("GET");
+                    if(connection.getResponseCode() == HTTP_OK){
+
+                        StringBuilder html = new StringBuilder();
+
+                        InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                        BufferedReader br = new BufferedReader(isr);
+
+                        String singleLine;
+                        while((singleLine = br.readLine()) != null)
+                            html.append(singleLine);
+
+                        publishProgress(html.toString());
+
+                        return html.toString();
+                    }
+
+                }catch(Exception e){
+
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            // control view component like handler
+            @Override
+            protected void onProgressUpdate(String... values) {
+                super.onProgressUpdate(values);
+
+                String targetHtml = values[0];
+                tvHtml.setText(targetHtml);
+            }
+
+            //
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+
+                if(s==null)
+                    Toast.makeText(MainActivity.this, "Download Failed...", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Download Complete!", Toast.LENGTH_SHORT).show();
+
+
+                // TODO - Progress bar dismissed
+            }
+        }.execute();
+
     }
 
     @Override
     public void onClick(View v) {
 
-        switch(v.getId()){
+        switch (v.getId()) {
 
             case R.id.button_download:
 
-                String html = downloadHtml();
-                tvHtml.setText(html);
+                downloadHtml();
 
                 break;
 
         }
 
     }
+
 }
