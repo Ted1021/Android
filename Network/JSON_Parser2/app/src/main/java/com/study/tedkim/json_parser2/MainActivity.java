96363eee86f656df1dce0f4152b32f711fad948b
Ted <kimtx2017@gmail.com>
2017-06-07 20:50:09 +0900

    # Add Comment
    - Logic of JSON Object Parsing

--- Network/JSON_Parser2/app/src/main/java/com/study/tedkim/json_parser2/MainActivity.java
+++ Network/JSON_Parser2/app/src/main/java/com/study/tedkim/json_parser2/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    // JSON Array 내에 있는 JSON Object 들에 접근하는 메소드
     public void parsingJsonObject(){
 
         String product, maker, price, result;
@@ -56,17 +57,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         try{
 
+            // 1. JSON Array 가 기록 된 String 변수 명으로 JSON Array 객체를 생성한다
             JSONArray ja = new JSONArray(JSON);
+            // 2. JSON Array 내에 있는 JSON Object 에 순차적으로 접근한다
             for(int i=0; i<ja.length(); i++){
 
+                // 2.1 기존의 JSONObject 또한 String 변수명으로 객체를 생성해야 한다
+                // 하지만 getJSONObject 메소드는 JSON Array에 있는 Object 들을 접근하기 위한
+                // 메소드들로써 index 를 이용해 접근 가능하다
                 JSONObject jo = ja.getJSONObject(i);
 
+                // 2.2 JSON Object 내의 elements 들 또한 변수명을 key 값으로 삼아 데이터를 가져온다
                 product = jo.getString("Product");
                 maker = jo.getString("Maker");
                 price = jo.getString("Price");
 
+                // 2.3 순차적으로 가져온 데이터 결과값을 정리해 준다
                 result = "제품명 : " + product + " 제조사 : " + maker + " 가 격 : " + price + "\n";
-
                 sb.append(result);
 
             }
