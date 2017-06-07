ba6a14e560f20d132f75f5b4c9e43976c6350472
Ted <kimtx2017@gmail.com>
2017-06-07 19:56:20 +0900

    # Implement Download Manager

--- Network/Download_Manager/app/src/main/java/com/study/tedkim/download_manager/MainActivity.java
+++ Network/Download_Manager/app/src/main/java/com/study/tedkim/download_manager/MainActivity.java
@@ -1,17 +1,25 @@
 package com.study.tedkim.download_manager;
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnDownload;
 
     DownloadManager mDM;
-    long mId;
+    long mId = 0;
 
 
     @Override
@@ -19,9 +27,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDM = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         initView();
 
+    }
 
+    public void initView() {
+
+        btnDownload = (Button) findViewById(R.id.button_download);
+        btnDownload.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mId != 0) {
+            unregisterReceiver(mDownloadComplete);
+            mId = 0;
+        }
     }
 
     @Override
@@ -31,7 +55,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.button_download:
 
-                startDownloadManager();
+                new AsyncTask<Void, Void, Void>(){
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+
+                        startDownloadManager();
+                        return null;
+                    }
+
+                }.execute();
 
                 break;
 
@@ -41,13 +74,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void startDownloadManager() {
 
 
+        Uri uri = Uri.parse("http://soen.kr/data/child2.jpg");
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle("Test Download");
+        request.setDescription("Now on downloading image file from URL ...");
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+
+        mId = mDM.enqueue(request);
+
+        try{
+            Thread.sleep(3000);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("DownloadManager.ACTION_DOWNLOAD_COMPLETE");
+
+        registerReceiver(mDownloadComplete, intentFilter);
+
+        Log.e("TEST", ">>>>>>>>>>>> end");
     }
 
-    public void initView() {
 
-        btnDownload = (Button) findViewById(R.id.button_download);
-        btnDownload.setOnClickListener(this);
-    }
+    BroadcastReceiver mDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("DOWNLOAD", ">>>>>> Download Complete!! ");
+            Toast.makeText(MainActivity.this, "Download complete !!", Toast.LENGTH_LONG).show();
+
+        }
+    };
 
 
 }
