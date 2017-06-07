e8e4468ab72398418afff1997337423c3d97aadd
Ted <kimtx2017@gmail.com>
2017-06-07 20:42:41 +0900

    # Implement JSON Object Parser

--- Network/JSON_Parser2/app/src/main/java/com/study/tedkim/json_parser2/MainActivity.java
+++ Network/JSON_Parser2/app/src/main/java/com/study/tedkim/json_parser2/MainActivity.java
@@ -6,15 +6,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnParse;
     TextView tvResult;
 
+    static final String JSON = "[{\"Product\":\"Mouse\", \"Maker\":\"Samsung\", \"Price\":23000}," +
+                                "{\"Product\":\"KeyBoard\", \"Maker\":\"LG\", \"Price\":12000}," +
+                                "{\"Product\":\"HDD\", \"Maker\":\"WD\", \"Price\":156000}]";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
     }
 
     public void initView(){
@@ -41,6 +51,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void parsingJsonObject(){
 
+        String product, maker, price, result;
+        StringBuilder sb = new StringBuilder();
+
+        try{
+
+            JSONArray ja = new JSONArray(JSON);
+            for(int i=0; i<ja.length(); i++){
+
+                JSONObject jo = ja.getJSONObject(i);
+
+                product = jo.getString("Product");
+                maker = jo.getString("Maker");
+                price = jo.getString("Price");
+
+                result = "제품명 : " + product + " 제조사 : " + maker + " 가 격 : " + price + "\n";
+
+                sb.append(result);
+
+            }
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        tvResult.setText(sb);
 
     }
 }
